5d7e55078c6984dbb29d7dfcc6688b0825e97e50
MTanrivermis <tanrivermis.mehmet@gmail.com>
2024-09-24 02:17:03 +0300

    carousel added

---
+++ components/Carousel.jsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import Title from "./ui/Title";
+
+const Carousel = () => {
+  return (
+    <div className="h-screen w-full container mx-auto -mt-[88px]">
+      <div className="absolute top-0 left-0 h-full w-full">
+      <div className="relative h-full w-full">
+      <Image
+          src="/images/hero-bg.jpg"
+          alt=""
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
+      </div>
+      <div className="relative text-white top-48 flex flex-col items-start gap-y-10">
+        <Title addClass="text-6xl">Fast Food Restaurant</Title>
+        <p className="text-sm sm:w-2/4 w-full">
+          Doloremque, itaque aperiam facilis rerum, commodi, temporibus sapiente
+          ad mollitia laborum quam quisquam esse error unde. Tempora ex
+          doloremque, labore, sunt repellat dolore, iste magni quos nihil
+          ducimus libero ipsam.
+        </p>
+        <button className="btn btn-primary">Order Now</button>
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
--- components/ui/Search.jsx
+++ components/ui/Search.jsx
@@ -1,73 +1,3 @@
-// import OutsideClickHandler from "react-outside-click-handler";
-// import Title from "./Title";
-// import Image from "next/image";
-// import { GiCancel } from "react-icons/gi";
-
-
-
-// const Search = ({ setIsSearchModal }) => {
-//   return (
-//     <div className="fixed top-0 left-0 w-screen h-screen z-50 after:content-[''] after:w-screen after:h-screen after:bg-white after:absolute after:top-0 after:left-0 after:opacity-60 grid place-content-center ">
-//       <OutsideClickHandler onOutsideClick={() => setIsSearchModal(false)}>
-//         <div className="w-full h-full grid place-content-center relative">
-//           <div className="relative z-50 w-[600px] h-[600px] bg-white border-2 p-5">
-//             <Title addClass="text-[40px] text-center">Search</Title>
-//             <input
-//               type="text"
-//               placeholder="Search..."
-//               className="border w-full my-10 h-[42px] rounded-xl "
-//             />
-//               <ul>
-//                 <li className="flex items-center justify-between p-1 hover:bg-primary transition-all">
-//                   <div className="relative flex">
-//                     <Image
-//                       src="/images/f1.png"
-//                       alt=""
-//                       width={48}
-//                       height={48}
-//                     />
-//                   </div>
-//                   <span className="font-bold">Good Pizza</span>
-//                   <span className="font-bold">$10</span>
-//                 </li>
-//                 <li className="flex items-center justify-between p-1 hover:bg-primary transition-all">
-//                   <div className="relative flex">
-//                     <Image
-//                       src="/images/f1.png"
-//                       alt=""
-//                       width={48}
-//                       height={48}
-//                     />
-//                   </div>
-//                   <span className="font-bold">Can Burger</span>
-//                   <span className="font-bold">$15</span>
-//                 </li>
-//                 <li className="flex items-center justify-between p-1 hover:bg-primary transition-all">
-//                   <div className="relative flex">
-//                     <Image
-//                       src="/images/f1.png"
-//                       alt=""
-//                       width={48}
-//                       height={48}
-//                     />
-//                   </div>
-//                   <span className="font-bold">Drink</span>
-//                   <span className="font-bold">$10</span>
-//                 </li>
-//               </ul>
-//               <button className="absolute top-4 right-4" onClick={()=> setIsSearchModal(false)}>
-//                 <GiCancel size={25} className="transition-all"/>
-//               </button>
-//           </div>
-//         </div>
-//       </OutsideClickHandler>
-//     </div>
-//   );
-// };
-
-// export default Search;
-
-
 import Image from "next/image";
 import React from "react";
 import OutsideClickHandler from "react-outside-click-handler";
@@ -78,7 +8,7 @@ const Search = ({ setIsSearchModal }) => {
     <div className="fixed top-0 left-0 w-screen h-screen z-50 after:content-[''] after:w-screen after:h-screen after:bg-white after:absolute after:top-0 after:left-0 after:opacity-60 grid place-content-center">
       <OutsideClickHandler onOutsideClick={() => setIsSearchModal(false)}>
         <div className="w-full h-full grid place-content-center relative">
-          <div className="relative z-50 w-[600px] h-[550px] bg-white border-2 p-5">
+          <div className="relative z-50 md:w-[600px] w-[370px] bg-white border-2 p-10 rounded-3xl">
             <Title addClass="text-[40px] text-center">Search</Title>
             <input
               type="text"
