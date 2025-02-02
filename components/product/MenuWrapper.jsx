b968cc627a918598aae4211ce3b61d207a2ea3f0
MTanrivermis <tanrivermis.mehmet@gmail.com>
2025-02-02 17:51:21 +0300

    up

--- components/product/MenuWrapper.jsx
+++ components/product/MenuWrapper.jsx
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Title from "../ui/Title";
 import MenuItem from "./MenuItem";
 
 const MenuWrapper = ({ categoryList, productList }) => {
   const [active, setActive] = useState(0);
   const [filter, setFilter] = useState([]);
+  const [productLimit, setProductLimit] = useState(3);
 
   useEffect(() => {
     setFilter(
@@ -13,11 +14,11 @@ const MenuWrapper = ({ categoryList, productList }) => {
           product.category === categoryList[active].title.toLowerCase()
       )
     );
-  }, [productList, categoryList, active]);
+  }, [categoryList, productList, active]);
 
   return (
-    <div className="container mx-auto mb-16">
-      <div className="flex flex-col items-center ">
+    <div className="container mx-auto  mb-16">
+      <div className="flex flex-col items-center w-full">
         <Title addClass="text-[40px]">Our Menu</Title>
         <div className="mt-10">
           {categoryList &&
@@ -27,18 +28,29 @@ const MenuWrapper = ({ categoryList, productList }) => {
                   index === active && "bg-secondary text-white"
                 }`}
                 key={category._id}
-                onClick={() => setActive(index)}
+                onClick={() => {
+                  setActive(index);
+                  setProductLimit(3);
+                }}
               >
                 {category.title}
               </button>
             ))}
         </div>
       </div>
-      <div className="mt-8 grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4 min-h-[400px]">
-        {filter?.length > 0 &&
-          filter.map((product) => (
-            <MenuItem key={product._id} product={product} />
-          ))}
+      <div className="mt-8 grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4 min-h-[450px]">
+        {filter.length > 0 &&
+          filter
+            .slice(0, productLimit)
+            .map((product) => <MenuItem key={product._id} product={product} />)}
+      </div>
+      <div className="flex items-center justify-center w-full mt-8">
+        <button
+          className="btn-primary"
+          onClick={() => setProductLimit(productLimit + 3)}
+        >
+          View More
+        </button>
       </div>
     </div>
   );
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,16 +1,21 @@
+import Layout from "@/layout/Layout";
+import store from "../redux/store";
+import Router from "next/router";
+import NProgress from "nprogress";
+
+import "nprogress/nprogress.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-
+import "react-toastify/dist/ReactToastify.css";
 import "../styles/globals.css";
-import Layout from "@/layout/Layout";
 
 import { Provider } from "react-redux";
-import store from "../redux/store";
-
 import { SessionProvider } from "next-auth/react";
-
 import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 export default function App({
   Component,
