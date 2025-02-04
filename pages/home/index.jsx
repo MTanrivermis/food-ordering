6e50de78d4471f2f43a2ba391683d1450fe3608d
MTanrivermis <tanrivermis.mehmet@gmail.com>
2025-02-04 23:07:09 +0300

    up

--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -3,8 +3,8 @@ import Carousel from "@/components/Carousel";
 import MenuWrapper from "@/components/product/MenuWrapper";
 import About from "@/components/About";
 import Reservation from "../../components/Reservation";
-import Customers from "@/components/customers/customers";
 import React from "react";
+import Customers from "@/components/customers/Customers";
 
 const Index = ({ categoryList, productList }) => {
   return (
